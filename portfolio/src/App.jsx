6801ffda08a7556bd557142d6d4f0fecc67508e6
onys <thkim2017@gmail.com>
2022-06-07 00:21:33 +0900

    refactor: 💡 MainPage 새페이지로 분리

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,11 @@
+import MainPage from './MainPage';
 import './App.scss';
 import applicantImg from "./static/images/applicant-img.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className='applicant-name'>
-        <div>
-          Taeho
-        </div>
-        <div>
-          Kim
-        </div>
-      </div>
-      <div>
-        <img src={applicantImg} alt="" />
-      </div>
-      <div className='job-objective'>
-        <div>
-          Front-End Developer
-        </div>
-      </div>
+      <MainPage />
     </div>
   );
 }
---
+++ portfolio/src/MainPage.jsx
@@ -0,0 +1,25 @@
+import applicantImg from "./static/images/applicant-img.svg";
+import "./MainPage.scss";
+
+export default function MainPage() {
+  return (
+    <div className='MainPage'>
+      <div className='applicant-name'>
+        <div>
+          Taeho
+        </div>
+        <div>
+          Kim
+        </div>
+      </div>
+      <div>
+        <img src={applicantImg} alt="" />
+      </div>
+      <div className='job-objective'>
+        <div>
+          Front-End Developer
+        </div>
+      </div>
+    </div>
+  )
+}
