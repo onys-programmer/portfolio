b51700de09573717017e048ce72b689f5f566c45
onys <thkim2017@gmail.com>
2022-06-06 23:57:29 +0900

    feat: 🎸 applicant img 추가

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.scss';
+import applicantImg from "./static/images/applicant-img.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className='developer-name'>
+      <div className='applicant-name'>
         <div>
           Taeho
         </div>
         <div>
           Kim
         </div>
+      </div>
+      <div>
+        <img src={applicantImg} alt="" />
       </div>
     </div>
   );
