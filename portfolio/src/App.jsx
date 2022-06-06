5ae8404dd16340ce74cf48a5bb9d3b8002755350
onys <thkim2017@gmail.com>
2022-06-06 23:25:27 +0900

    feat: 🎸 초기 앱 세팅
    eslint 및 gitignore 설정

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div>
+          Taeho
+        </div>
+        <div>
+          Kim
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
