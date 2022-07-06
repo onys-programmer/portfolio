d8a0706d77d6490dfcc0909f1f75bce4a9e6a32e
onys <thkim2017@gmail.com>
2022-07-06 13:39:34 +0900

    pull from main branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {
   Route,
 } from "react-router-dom";
 import MainPage from './MainPage';
-import React from 'react';
 import './App.scss';
 
 function App() {
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import applicantImg from "./static/images/applicant-img.svg";
 import "./MainPage.scss";
 
