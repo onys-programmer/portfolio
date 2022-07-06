07232f07a25710ef7e358d8740443de0a90bb1fa
onys <thkim2017@gmail.com>
2022-07-06 22:45:43 +0900

    feat: 🎸 component 위치 설덩

--- src/pages/MainPage/SittingApplicant.jsx
+++ src/pages/MainPage/SittingApplicant.jsx
@@ -5,9 +5,11 @@ export default function SittingApplicant() {
   return (
     <div className="sitting-applicant">
       <img className="applicant-img" src={applicantImg} alt="" />
-      <div className="job-objective">
-        <div className="job-objective-text">
-          Front-End Developer
+      <div className="chair">
+        <div className="chair-box">
+          <div className="chair-text">
+            Front-End Developer
+          </div>
         </div>
       </div>
     </div>
