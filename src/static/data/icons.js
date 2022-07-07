0e4dede96512fd2c97e096853c8b640e64d7a4c0
onys <thkim2017@gmail.com>
2022-07-07 15:01:36 +0900

    feat: 🎸 icons 5개 구현

--- src/static/data/icons.js
+++ src/static/data/icons.js
@@ -1,4 +1,9 @@
 import InsightsIcon from '@mui/icons-material/Insights';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import BoltIcon from '@mui/icons-material/Bolt';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import BackpackIcon from '@mui/icons-material/Backpack';
+
 import IconImg from '../../components/TaskBar/IconImg';
 
 const icons = [
@@ -9,8 +14,23 @@ const icons = [
   },
   {
     name: 'Projects',
-    img: () => <IconImg Component={InsightsIcon} />,
+    img: () => <IconImg Component={CardGiftcardIcon} />,
     shortCutKey: 'p',
+  },
+  {
+    name: 'Skill',
+    img: () => <IconImg Component={BoltIcon} />,
+    shortCutKey: 'k',
+  },
+  {
+    name: 'Career',
+    img: () => <IconImg Component={AssignmentIndIcon} />,
+    shortCutKey: 'c',
+  },
+  {
+    name: 'Inventory',
+    img: () => <IconImg Component={BackpackIcon} />,
+    shortCutKey: 'i',
   },
 ];
 
