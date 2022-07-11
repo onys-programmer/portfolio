8010d95fa1b6f3130f52122f27dcede3717bbce8
onys <thkim2017@gmail.com>
2022-07-11 11:09:23 +0900

    feat: 🎸 implement shortcut key

---
+++ src/components/TaskBar/ShortCutKey.jsx
@@ -0,0 +1,9 @@
+import './ShortCutKey.scss';
+
+export default function ShortCutKey() {
+  return (
+    <div className="ShortCutKey">
+      key
+    </div>
+  );
+}
--- src/components/TaskBar/TaskBar.jsx
+++ src/components/TaskBar/TaskBar.jsx
@@ -1,11 +1,17 @@
 import './TaskBar.scss';
 import ShortCutIcon from './ShortCutIcon';
-import icons from '../../static/data/icons';
+import ShortCutKey from './ShortCutKey';
 
+import icons from '../../static/data/icons';
 export default function TaskBar() {
   return (
     <footer className='TaskBar'>
-      {icons.map(icon => <ShortCutIcon key={icon["name"]} icon={icon} />)}
+      <div className='short-cut-keys'>
+        {icons.map(icon => <ShortCutKey key={icon["key"]} icon={icon} />)}
+      </div>
+      <div className='short-cut-icons'>
+        {icons.map(icon => <ShortCutIcon key={icon["name"]} icon={icon} />)}
+      </div>
     </footer>
   );
 }
