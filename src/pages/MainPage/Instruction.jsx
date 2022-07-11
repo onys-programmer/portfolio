5052b5621d4c4e49763b2ab21de33e12bf5806e2
onys <thkim2017@gmail.com>
2022-07-11 14:08:16 +0900

    feat: 🎸 Press shortcut keys 문구 추가

---
+++ src/pages/MainPage/Instruction.jsx
@@ -0,0 +1,14 @@
+import './Instruction.scss';
+
+export default function Instruction() {
+  return (
+    <section className='Instruction'>
+      <p>
+        Press shortcut keys
+      </p>
+      <p className="detail">
+        To Explore |
+      </p>
+    </section>
+  );
+}
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,14 @@
-import "./MainPage.scss";
 import Name from './Name';
+import Instruction from "./Instruction";
 import SittingApplicant from "./SittingApplicant";
+
+import "./MainPage.scss";
 
 export default function MainPage() {
   return (
     <main className='MainPage'>
       <Name />
+      <Instruction />
       <SittingApplicant />
     </main>
   );
